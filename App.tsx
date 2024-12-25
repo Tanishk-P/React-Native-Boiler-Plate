589bc42d5c70bcaf9b4d94cfd39f670952bdb3ce
Tanishk <tanishkparadkar@gmail.com>
2024-12-25 21:21:50 +0530

    Added the Stack.

--- App.tsx
+++ App.tsx
@@ -1,111 +1,39 @@
-import type {PropsWithChildren} from 'react';
+/* eslint-disable react/no-unstable-nested-components */
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {HomeScreen} from './src/screens/Home/home-screen';
+import {NetworkLoggerScreen} from './src/screens/Network-Logger/network.logger-screen';
+import {AppStackParams} from './src/utils/app-stack-params-list';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export function App() {
+  const Stack = createStackNavigator<AppStackParams>();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  const ScreenNavigator = (): JSX.Element => (
+    <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          header: () => null,
+        }}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Stack.Screen
+        name="NetworkLoggerScreen"
+        component={NetworkLoggerScreen}
+        options={{
+          header: () => null,
+        }}
+      />
+    </Stack.Navigator>
+  );
+
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <ScreenNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
--- src/screens/Network-Logger/network.logger-screen.tsx
+++ src/screens/Network-Logger/network.logger-screen.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
 import NetworkLogger from 'react-native-network-logger';
+import {safeAreaStyles} from '../../styles/safeArea.styles';
 
 export function NetworkLoggerScreen(): JSX.Element {
-  return <NetworkLogger theme="dark" />;
+  return (
+    <SafeAreaView style={safeAreaStyles.fullScreen}>
+      <NetworkLogger theme="dark" />;
+    </SafeAreaView>
+  );
 }
