5609ed4968e07a8cd3257d2a16165596f645c75a
Tanishk <tanishkparadkar@gmail.com>
2024-12-25 21:32:50 +0530

    Added the Tab Navigator

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {HomeScreen} from './src/screens/Home/home-screen';
 import {NetworkLoggerScreen} from './src/screens/Network-Logger/network.logger-screen';
+import navigationService from './src/services/navigation.service';
 import {AppStackParams} from './src/utils/app-stack-params-list';
 
 const Stack = createStackNavigator<AppStackParams>();
+const Tab = createBottomTabNavigator<AppStackParams>();
 
 const ScreenNavigator = (): JSX.Element => (
   <Stack.Navigator initialRouteName="HomeScreen">
     <Stack.Screen
       name="HomeScreen"
-      component={HomeScreen}
+      component={TabNavigator}
       options={{
         header: () => null,
       }}
@@ -27,10 +30,25 @@ const ScreenNavigator = (): JSX.Element => (
   </Stack.Navigator>
 );
 
+const TabNavigator = (): JSX.Element => (
+  <Tab.Navigator
+    initialRouteName="HomeScreen"
+    screenOptions={{tabBarShowLabel: false}}>
+    <Tab.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{
+        header: () => null,
+        tabBarLabel: 'Home',
+      }}
+    />
+  </Tab.Navigator>
+);
+
 export function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationService.assignNavigator}>
         <ScreenNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
