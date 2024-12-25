a433d78dd87af82a4eb06c4c4460476e22de852a
Tanishk <tanishkparadkar@gmail.com>
2024-12-25 20:02:33 +0530

    Setup Folder Structure, and environment

---
+++ src/utils/app-stack-params-list.ts
@@ -0,0 +1,10 @@
+export type AppStackParams = {
+  // Put your applications screens in here
+  SplashScreen: undefined;
+  NetworkLoggerScreen: undefined; // We added this to log the api calls
+};
+
+export const AppStackParamsList: AppStackParams = {
+  SplashScreen: undefined,
+  NetworkLoggerScreen: undefined,
+};
---
+++ src/utils/colors.ts
@@ -0,0 +1,3 @@
+export const colors = {
+  // Put your colors in here
+};
